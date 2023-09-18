c0d8f26de7cad7dbd209f255b90348ff9d88bdac
Jayzilva <10748102@students.plymouth.ac.uk>
2023-09-19 00:10:41 +0530

    Update init

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,15 @@ app = Flask(__name__)  #instantiate Flask application & passing the module name
 @app.route('/') #passing the endpoint as  paramenter
 def index(): #view funtion under the decorater
     return "<h1>Hello World</h1>"
+
+@app.route("/home")
+def home():
+    return "<h2>The Home Page</h2>"
     
+@app.route("/place/<place>")
+def location(place):
+    return "<h2>You are on the" + place +"Page</h2>"
+
 if __name__ == "__main__": #if call directly from the CMD
     app.run(debug=True)
     
