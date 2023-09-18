9dbb5ffb1d1f86d5028b1c283a91b398eb62f6ad
Jayzilva <10748102@students.plymouth.ac.uk>
2023-09-19 00:27:09 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)  #instantiate Flask application & passing the module name
@@ -8,13 +8,10 @@ app = Flask(__name__)  #instantiate Flask application & passing the module name
 def index(): #view funtion under the decorater
     return "<h1>Hello World</h1>"
 
-@app.route("/home")
+@app.route("/home", methods=["GET" , "POST"])
 def home():
-    return "<h2>The Home Page</h2>"
-    
-@app.route("/place/<place>")
-def location(place):
-    return "<h2>You are on the " + place +" Page</h2>"
+    return render_template("index.html", myvar= "Varible passed in") #render_templates look at the templates file
+
 
 if __name__ == "__main__": #if call directly from the CMD
     app.run(debug=True)
