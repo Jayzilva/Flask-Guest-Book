5ea66ec7d79814a54821cdbf8de7d6831ee41de3
Jayzilva <10748102@students.plymouth.ac.uk>
2023-09-19 11:33:41 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,15 @@
 from flask import Flask, render_template
 
+app = Flask(__name__)
 
-app = Flask(__name__)  #instantiate Flask application & passing the module name
-    
-    #creating a route
-@app.route('/') #passing the endpoint as  paramenter
-def index(): #view funtion under the decorater
-    return "<h1>Hello World</h1>"
+@app.route('/')
+def index():
+	return render_template('index.html')
 
-@app.route("/home", methods=["GET" , "POST"])
-def home():
-    links = ["https://www.youtube.com/" , "https://www.canva.com/" , "http://127.0.0.1:5000/" , "https://github.com/"]
-    
-    return render_template("index.html", myvar="Var x" ,links=links) #render_templates look at the templates file
+@app.route('/sign')
+def sign():
+	return render_template('sign.html')
 
 
-if __name__ == "__main__": #if call directly from the CMD
-    app.run(debug=True)
-    
+if __name__ == '__main__':
+	app.run(debug=True)
