17da93dea1d811489cc159911bd16f8614fdc7b7
Jayzilva <10748102@students.plymouth.ac.uk>
2023-09-20 13:24:39 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,11 @@ def index():
 def sign():
 	return render_template('sign.html')
 
+@app.route("/process", methods= ['POST'])
+def process():
+    name = request.form["name"]
+    comment = request.form["comment"]
+    return render_template("index.html", name= name , comment = comment)
 
 if __name__ == '__main__':
 	app.run(debug=True)
